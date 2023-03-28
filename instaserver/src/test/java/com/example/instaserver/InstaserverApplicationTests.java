53eb27e3b591c8741be7595ca3329ef8c7040e7c
heon98 <duafogjs@naver.com>
2023-03-28 15:07:41 +0900

    asdF

---
+++ instaserver/src/test/java/com/example/instaserver/InstaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
